fd4083e2828ff4ba43332e6d27f50593afc5e06c
Jerome Dobbels <jerome.dobbels@epitech.eu>
2019-03-18 16:47:24 +0100

    feat(Get User): Get a user with his id.

--- src/main/java/com/epitech/linkedinspringbootneo4jv1/controller/UserController.java
+++ src/main/java/com/epitech/linkedinspringbootneo4jv1/controller/UserController.java
@@ -4,19 +4,25 @@ import com.epitech.linkedinspringbootneo4jv1.model.User;
 import com.epitech.linkedinspringbootneo4jv1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("/")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @GetMapping("/users")
     public Collection<User> getAll() {
         return userService.getAll();
     }
--- src/main/java/com/epitech/linkedinspringbootneo4jv1/service/UserService.java
+++ src/main/java/com/epitech/linkedinspringbootneo4jv1/service/UserService.java
@@ -13,6 +13,11 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    public User findById(Long id) {
+        User result = this.userRepository.findById(id).orElseGet(null);
+        return result;
+    }
+
     public Collection<User> getAll() {
         return userRepository.getAllUsers();
     }
