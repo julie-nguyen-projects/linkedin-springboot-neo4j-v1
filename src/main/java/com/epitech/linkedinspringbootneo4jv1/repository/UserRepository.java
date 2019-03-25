30bca7728ff1c63884131c416e5b8d961e586153
julie-nguyen-projects <julie_nguyen@live.fr>
2019-03-25 13:10:09 +0100

    feat(City / Users): get users by city

--- src/main/java/com/epitech/linkedinspringbootneo4jv1/repository/UserRepository.java
+++ src/main/java/com/epitech/linkedinspringbootneo4jv1/repository/UserRepository.java
@@ -18,4 +18,7 @@ public interface UserRepository extends Neo4jRepository<User, Long> {
 
     @Query("MATCH (u:User)-[li:LIVES_IN]->(ci:City)-[ii:IS_IN]->(co:Country) RETURN u,li,ci,ii,co")
     Collection<User> getAllUsers();
+
+    @Query("MATCH (co:Country)<-[ii:IS_IN]-(c:City)<-[li:LIVES_IN]-(u:User) WHERE c.name={cityName} RETURN u,c,li,co,ii")
+    Collection<User> getAllUsersByCity(String cityName);
 }
