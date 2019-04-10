459b5a0cefe3533da5cf38d1298afa8b67b54e21
Jerome Dobbels <jerome.dobbels@epitech.eu>
2019-04-10 17:03:16 +0200

    QUAL(City/Country) Javadoc added.

--- src/main/java/com/epitech/linkedinspringbootneo4jv1/controller/CityController.java
+++ src/main/java/com/epitech/linkedinspringbootneo4jv1/controller/CityController.java
@@ -8,26 +8,48 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+/**Controller for City class */
 @RestController
 @RequestMapping("/")
 public class CityController {
 
-    @Autowired
-    CityService cityService;
 
+    private final CityService cityService;
+
+    /**
+     * CONSTRUCTOR
+     * @param cityService
+     */
+    @Autowired
+    public CityController(CityService cityService) { this.cityService = cityService; }
+
+    /**
+     * Get a city by name
+     * @param name The name of the city
+     * @return The city found
+     */
     @GetMapping("/cities/name/{name}")
     public City getByName(@PathVariable("name") String name) { return this.cityService.getByName(name); }
 
+    /**
+     * Get all cities
+     * @return Collection of all cities
+     */
     @GetMapping("/cities")
     public Collection<City> getAll() { return this.cityService.getAll(); }
 
+    /**
+     * Get all cities by country
+     * @param coutryName The name of the country
+     * @return A collection of cities
+     */
     @GetMapping("/cities/country/{countryName}")
     public Collection<City> getAllByCountry(@PathVariable("countryName")String coutryName) {
         return this.cityService.getAllbyCountry(coutryName);
     }
 
     /**
-     * Create a user
+     * Create a city
      * @param city : city to create
      * @return city created
      */
@@ -36,11 +58,21 @@ public class CityController {
         return cityService.createCity(city);
     }
 
+    /**
+     * Update a city
+     * @param id The id of the existed city
+     * @param city The new datas of the city
+     * @return The updated city
+     */
     @PutMapping("/cities/id/{id}")
     public City updateCity (@PathVariable Long id, @RequestBody City city) {
         return this.cityService.updateCity(id, city);
     }
 
+    /**
+     * Delete a city
+     * @param id The id of the city
+     */
     @DeleteMapping("/cities/id/{id}")
     public void deleteCity(@PathVariable("id") Long id) { this.cityService.deleteCity(id); }
 }
--- src/main/java/com/epitech/linkedinspringbootneo4jv1/controller/CountryController.java
+++ src/main/java/com/epitech/linkedinspringbootneo4jv1/controller/CountryController.java
@@ -11,24 +11,59 @@ import java.util.Collection;
 @RequestMapping("/")
 public class CountryController {
 
-    @Autowired
-    CountryService countryService;
+    private final CountryService countryService;
 
+    /**
+     * CONSTRUCTOR
+     * @param countryService
+     */
+    @Autowired
+    public CountryController(CountryService countryService) { this.countryService = countryService; }
+
+    /**
+     * Get all coutry
+     * @return Collection of country
+     */
     @GetMapping("/countries")
     public Collection<Country> getAll() { return this.countryService.getAllCountries(); }
 
+    /**
+     * Get a country by id
+     * @param id The id of the country
+     * @return the country found
+     */
     @GetMapping("/countries/id/{id}")
     public Country getById(@PathVariable("id") Long id) { return this.countryService.getById(id); }
 
+    /**
+     * Get a country by name
+     * @param name The name of the country
+     * @return the country found
+     */
     @GetMapping("/countries/name/{name}")
     public Country getByName(@PathVariable("name") String name) { return this.countryService.getByName(name); }
 
+    /**
+     * Create a new country
+     * @param country
+     * @return The country created
+     */
     @PostMapping("/countries")
     public Country addCountry(@RequestBody Country country) { return this.countryService.createCountry(country); }
 
+    /**
+     * Update an existing country
+     * @param id The id of the existing country
+     * @param country the new value of the country
+     * @return The updated country
+     */
     @PutMapping("/countries/id/{id}")
     public Country updateCountry(@PathVariable("id") Long id, @RequestBody Country country) { return this.countryService.updateCountry(id, country); }
 
+    /**
+     * Delete a country
+     * @param id The id of the country
+     */
     @DeleteMapping("/countries/id/{id}")
     public void deleteCountry(@PathVariable("id") Long id) { this.countryService.deleteCountry(id); }
 }
